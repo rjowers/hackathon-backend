4b1088b8882ce8f2a8ced3360fa318e1afc1817d
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-16 14:38:27 -0400

    added user table and salt password is working

---
+++ server/controllers/users.js
@@ -0,0 +1,51 @@
+const Users = require("../models").users;
+const bcrypt = require('bcryptjs');
+// const jwt = require('jwt-simple');
+const appSecrets = require ('../config/secret');
+
+module.exports = {
+   create (req, res) {
+     var salt = bcrypt.genSaltSync(10);
+    var hashedPass = bcrypt.hashSync(req.body.password, salt);
+    Users.create({
+      username: req.body.username,
+      password: hashedPass,
+      email: req.body.email,
+      salt: salt
+
+    })
+      .then(users => res.status(201).send(users))
+      .catch(error => res.status(400).send(error));
+   }
+
+  //  login (req, res) {
+  //       Users.findOne({
+  //         where: {
+  //           email: req.body.email
+  //         }
+  //       })
+  //         .then(user => {
+  //           if (!user) {
+  //             return res.status(401).send({ message: "No such email or wrong password." });
+  //           }
+   //
+  //           console.log(user.salt);
+  //           var input = bcrypt.hashSync(req.body.password, user.salt);
+  //           console.log(`hashed input: ${input}, stored password: ${user.password}`);
+  //           if (input === user.password) {
+  //             console.log('hello from inside input')
+  //             var token = jwt.encode({ id: user.id, username: user.username }, appSecrets.secret);
+  //             console.log(token)
+   //
+  //             var json = {
+  //             user: user,
+  //             token: token
+  //             };
+  //             return res.status(200).send(json);
+  //           } else {
+  //             return res.status(401).send({ message: "No such email or wrong password." });
+  //           }
+  //         })
+  //         .catch(error => res.status(400).send(error));
+  //      }
+     };
---
+++ server/models/users.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var users = sequelize.define('users', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    salt: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return users;
+};
