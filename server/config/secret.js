66ccea0fe66847a1cddd7e08a38a344575436e16
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-16 15:04:21 -0400

    Heroku is online

--- server/config/secret.js
+++ server/config/secret.js
@@ -1,7 +1,3 @@
-
 module.exports = {
-
-    secret: 'ilovescotchyscotch',
-    // 'database': 'mongodb://noder:noderauth&54;proximus.modulusmongo.net:27017/so9pojyN'
-
-};
+  secret: process.env.APP_SECRET
+}
