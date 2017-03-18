0ae31a3955f6d2d3b408adf21595e2e975d1288d
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-18 14:32:09 -0400

    added a profile photo column

---
+++ server/migrations/20170318181856-AddPhotourl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+
+      return queryInterface.addColumn('users', 'profilephoto', Sequelize.STRING);
+
+  },
+
+  down: function (queryInterface, Sequelize) {
+
+    return queryInterface.removeColumn('users', 'profilephoto', Sequelize.STRING);
+
+  }
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -16,6 +16,7 @@ module.exports = (app) => {
   app.get('/user/:userid/photos', UserController.findOne);
 
   app.post('/photos', Middleware.authenticate, PhotosController.create);
+  // app.post('/photos', Middleware.authenticate, PhotosController.create);
   app.get('/photos', PhotosController.findAll);
   app.delete('/photos/:photoid', PhotosController.delete);
   // app.get('/photos/:photoid/comments/', PhotosController.findComments);
