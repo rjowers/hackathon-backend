12a6fb56a326c2029e8feb12735b059258477f6f
Ryan Jowers <rjowers@g.clemson.edu>
2017-03-18 14:37:29 -0400

    added midware

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,14 +13,14 @@ module.exports = (app) => {
   app.post('/login', UserController.login);
   app.get('/users', UserController.findAll);
   app.delete('/users/:userid', UserController.delete);
-  app.get('/user/:userid/photos', UserController.findOne);
+  app.get('/user/:userid/photos', Middleware.authenticate, UserController.findOne);
 
   app.post('/photos', Middleware.authenticate, PhotosController.create);
   // app.post('/photos', Middleware.authenticate, PhotosController.create);
   app.get('/photos', PhotosController.findAll);
   app.delete('/photos/:photoid', PhotosController.delete);
   // app.get('/photos/:photoid/comments/', PhotosController.findComments);
-  app.get('/photo/:photoid/', PhotosController.findOne);
+  app.get('/photo/:photoid/', Middleware.authenticate, PhotosController.findOne);
 
 
 
